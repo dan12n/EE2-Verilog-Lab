955146b6afe0f64c21ae35392762935b62420984
dan12n <dan12n@users.noreply.github.com>
2016-12-12 19:28:34 +0000

    Update LFSR.v

--- Part_2/EX8/LFSR.v
+++ Part_2/EX8/LFSR.v
@@ -1,17 +1,18 @@
 //LFSR13
 module lfsr13 (data_out, clk, enable);
-	output reg [12:0] data_out;
+	output reg [13:0] data_out;
 	input clk, enable;
-	reg [12:0] sreg;
+	reg [5:0] sreg;
+	wire constant = 8'd250;
 
-	initial sreg = 7'd1;
-	initial data_out = 12'd0;
+	initial sreg = 6'd1;
+	initial data_out = 13'd0;
 	
 	always @(posedge clk)
 	if (enable == 1'b1) begin
-		sreg 		<= {sreg[11:0], sreg[0] ^ sreg[12]};
+		sreg 		<= {sreg[4:0], sreg[0] ^ sreg[5]};
 	
-		data_out 	<= sreg;
+		data_out 	<= sreg*constant;
 	end
 
 endmodule // lfsr13
