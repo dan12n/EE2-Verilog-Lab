f1cc8dd1bf670a26c49645ea49b62c7dcbd7303e
dan12n <dan12n@users.noreply.github.com>
2016-12-12 19:25:03 +0000

    Update EX8_top.v

--- Part_2/EX8/EX8_top.v
+++ Part_2/EX8/EX8_top.v
@@ -4,7 +4,7 @@ module EX8_top (CLOCK_50, KEY, HEX0, HEX1, HEX2, HEX3, HEX4, LEDR);
 	output[9:0] LEDR;
 	output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4;
 	
-	wire[12:0] prbs;
+	wire[13:0] prbs;
 	wire [3:0] BCD0, BCD1, BCD2, BCD3, BCD4;
 	
 	wire clk_div_50000, clk_div_2500, trigger, start_delay, time_out, en_lfsr;
@@ -12,9 +12,9 @@ module EX8_top (CLOCK_50, KEY, HEX0, HEX1, HEX2, HEX3, HEX4, LEDR);
 	divider_50000 	divideby5000 	(CLOCK_50, clk_div_50000);
 	divider_2500 	divideby2500 	(clk_div_50000, CLOCK_50, clk_div_2500);
 	
-	lfsr13			LFSR 			(prbs[12:0], clk_div_50000, en_lfsr);
+	lfsr13			LFSR 			(prbs[13:0], clk_div_50000, en_lfsr);
 	
-	EX8_delay       delay 			(prbs[12:0], clk_div_50000, start_delay, time_out);
+	EX8_delay       delay 			(prbs[13:0], clk_div_50000, start_delay, time_out);
 	
 	FSM 			finitestatem	(clk_div_50000, clk_div_2500, ~KEY[3], time_out, en_lfsr, start_delay, LEDR[9:0]);
 	
